5a4752c97129e3999057a15a38c2ebcbb2e8b189
Esumei Moses <archer.me24@gmail.com>
2024-03-13 01:34:53 -0700

    models/user

--- models/user.py
+++ models/user.py
@@ -11,8 +11,7 @@ class User(BaseModel):
     def __init__(self, *args, **kwargs):
         """ constructor method """
         super().__init__(*args, **kwargs)
-        self.email = ""
-        #self.email = kwargs.get('email', '')
+        self.email = kwargs.get('email', '')
         self.password = kwargs.get('password', '')
         self.first_name = kwargs.get('first_name', '')
         self.last_name = kwargs.get('last_name', '')
