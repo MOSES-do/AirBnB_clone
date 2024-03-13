034a44083cce8314d4833558fe6d220adfd4588f
Esumei Moses <archer.me24@gmail.com>
2024-03-13 01:25:07 -0700

    pycode requirement updated

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """User Class"""
 from models.base_model import BaseModel
@@ -11,7 +11,8 @@ class User(BaseModel):
     def __init__(self, *args, **kwargs):
         """ constructor method """
         super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email', '')
+        self.email = ""
+        #self.email = kwargs.get('email', '')
         self.password = kwargs.get('password', '')
         self.first_name = kwargs.get('first_name', '')
         self.last_name = kwargs.get('last_name', '')
